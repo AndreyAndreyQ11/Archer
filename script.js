118a91c5c1e53df3dca7159000eaeee78980d1ee
AndreyAndreyQ11 <aayy6491@gmail.com>
2022-12-08 14:58:20 +0200

    x2

--- script.js
+++ script.js
@@ -6,11 +6,25 @@ const tradition = document.querySelector('.Container_1 span');
 console.log(tradition);
 
 
-// tradition.style.top = "50%"
+// ГО традиция
 window.addEventListener("load", function (event) {
     // tradition.classList.add("test")
+
     tradition.style.cssText = `
     top: 50%;
     opacity: 1;
     `;
 });
+
+
+//
+
+const Menu = document.querySelector(".Menu")
+
+
+document.addEventListener("scroll", (ev) => {
+    let body = document.querySelector("body")
+
+    // console.dir(document);
+    console.log(body.scroolHeight);
+})
