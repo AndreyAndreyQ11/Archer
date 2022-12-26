4724e9ecc46586e733cabac7979a5003446004c3
AndreyAndreyQ11 <aayy6491@gmail.com>
2022-12-26 18:29:59 +0200

    test3

--- script.js
+++ script.js
@@ -3,17 +3,15 @@
 const tradition = document.querySelector('.Container_1 span');
 
 
-console.log(tradition);
-
-
 // ГО традиция
 window.addEventListener("load", function (event) {
+    tradition.classList.remove("start");
     // tradition.classList.add("test")
-
-    tradition.style.cssText = `
-    top: 50%;
-    opacity: 1;
-    `;
+    console.log(tradition);
+    // tradition.style.cssText = `
+    // top: 50%;
+    // opacity: 1;
+    // `;
 });
 
 
@@ -22,9 +20,8 @@ window.addEventListener("load", function (event) {
 const Menu = document.querySelector(".Menu")
 
 
-document.addEventListener("scroll", (ev) => {
-    let body = document.querySelector("body")
+// document.addEventListener("scroll", (ev) => {
+//     let body = document.querySelector("body")
 
-    // console.dir(document);
-    console.log(body.scroolHeight);
-})
+//     // console.dir(document);
+// })
