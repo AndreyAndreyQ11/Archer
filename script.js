256e7ac2542d888bc8caec138e537dc5b7ed37f9
AndreyAndreyQ11 <aayy6491@gmail.com>
2022-12-28 17:39:30 +0200

    test7

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 
 
 const tradition = document.querySelector('.Section_1 span');
-
-
 // ГО традиция
 window.addEventListener("load", function (event) {
     tradition.classList.remove("start");
-    // tradition.classList.add("test")
+
     console.log(tradition);
     // tradition.style.cssText = `
     // top: 50%;
@@ -14,14 +12,36 @@ window.addEventListener("load", function (event) {
     // `;
 });
 
-
-//
-
 const Menu = document.querySelector(".Menu")
-
 
 // document.addEventListener("scroll", (ev) => {
 //     let body = document.querySelector("body")
 
 //     // console.dir(document);
 // })
+///////////////////////////////////////
+
+
+const bundle = document.querySelector("#bundle");
+const options = {
+    rootMargin: '0px',
+    threshold: 0
+};
+
+
+
+const callback = function (entries, observer) {
+    console.log(entries);
+    console.log(observer);
+
+
+
+    // entries.forEach(element => {
+    //     // console.log("сработало_2", element, observer);
+    //     console.log("сработало_2", element.isIntersecting);
+    // });
+
+}
+
+let observer = new IntersectionObserver(callback, options);
+observer.observe(bundle);
